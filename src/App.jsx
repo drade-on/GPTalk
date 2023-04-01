85b3e3bb906e1d3dc16a888e8c2cb3c0b0df3cad
drade-on <albert.sibiga.dev@gmail.com>
2023-04-01 17:03:11 +0200

    Chat's and configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+
 import { useState } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-function App() {
+import Chat from "@/components/chat"
 
+function App() {
   return (
     <div className="App" >
       <BrowserRouter>
         <Routes>
-          <Route path="/chat" element={<Chat />} />
+          <Route path="/chat" element={<Chat />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
