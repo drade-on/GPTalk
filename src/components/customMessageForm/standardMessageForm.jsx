0326d717122ba1a24727b2bd025ce9f793d5b15b
drade-on <albert.sibiga.dev@gmail.com>
2023-04-04 03:49:20 +0200

    work on message form

---
+++ src/components/customMessageForm/standardMessageForm.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+import { PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import React, {useState} from 'react'
+import Dropzone from 'react-dropzone'
+
+function StandardMessageForm() {
+  const [message, setMessage] = useState("")
+  const [attachment, setAttachment] = useState("")
+  const [preview, setPreviev] = useState("")
+
+  const handleChange = (e)=>setMessage(e.target.value)
+
+  return (
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img src={preview} alt="preview" onLoad={() => URL.revokeObjectURL(preview)} className="message-form-preview-image" />
+          <XMarkIcon
+            className='message-form-icon-x'
+            onClick={() => {
+              setPreviev("")
+              setAttachment("")
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+          className='message-form-input'
+          type='text'
+          value={message}
+          onChange={handleChange}
+          placeholder='Send a message ...'
+          />
+        </div>
+        <div className="message-form-icons">
+          <Dropzone
+          acceptedFiles=".jpg,.jpeg,.png"
+          multiple={false}
+          noClick={true}
+          onDrop={(acceptedFiles)=>{
+            setAttachment(acceptedFiles[0])
+            setPreviev(URL.createObjectURL(acceptedFiles))
+          }}
+          >
+            {({getRootProps,getInputProps,open})=>(
+            <div{...getRootProps()}>
+               <input {...getInputProps()} /> 
+               <PaperClipIcon
+               className='message-form-icon-clip'
+               onClick={open}
+               />
+            </div>
+            )}
+          </Dropzone>
+          <hr className=''/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StandardMessageForm
